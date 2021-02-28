aaa76f0272b39735ed5bc653c9ffba727b8516bc
ayushdip <ayush.dip.eee17@itbhu.ac.in>
2021-02-28 12:48:38 +0530

    Basic server setup done
    Signed-off-by: ayushdip <ayush.dip.eee17@itbhu.ac.in>

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cricket app 
+
+### The app contains information of all cricket matches going on live. Through this app you can get all information about cricket matches. You can also browse a players statistics and other information
---
+++ src/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use('/',express.static(__dirname + '/public'));
+app.get('/hello',(req,res)=>{
+    res.send('Hello World');
+})
+app.listen(4444,()=>{
+    console.log('Server started at http://localhost:4444');
+})
